313585f1dceb280be4ce77eb9f4f7c5c0806230e
Trongluu <133474458+Trongluu@users.noreply.github.com>
2023-05-13 10:53:55 +0700

    Add files via upload

---
+++ Baikiemtra1/src/advance/dev/Person.java
@@ -0,0 +1,30 @@
+package advance.dev;
+
+Private String name;
+Private int age;
+Private String city;
+
+public Person(String name, int age,String city ) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public String getCity() {
+        return city;
+    }
+    
+    @Override
+    public String toString() {
+        return name + " (" + age + ") - " + city;
+    }
+}
+
